4b09674f80f212b3c24a4b9397d8520a16448962
AkshathaHolla91 <akshathaholla91@gmail.com>
2018-09-28 12:40:10 +0530

    added pragma once

--- include/event.hpp
+++ include/event.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <cstddef>
 #include <b64/encode.h>
--- include/eventsApi.hpp
+++ include/eventsApi.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include "restc-cpp/restc-cpp.h"
